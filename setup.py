03dba3e67a3c86ac6c1a5a3d3e4e0109968acd44
ryanwallace96 <ryanwallace@college.harvard.edu>
2017-12-09 23:06:57 -0500

    .travis and requirements.txt

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-packages = ['npdb',
-  			'npdb.core',
-  			'npdb.creation',
-  			'npdb.indexing',
-  			'npdb.linalg',
-  			'npdb.manipulation',
-  			'npdb.math',
-  			'npdb.random', 
-  			'npdb.sorting',
-  			'npdb.statistics']
+# manual package listing
+# packages = ['npdb',
+#             'npdb.core',
+#             'npdb.creation',
+#             'npdb.indexing',
+#             'npdb.linalg',
+#             'npdb.manipulation',
+#             'npdb.math',
+#             'npdb.random', 
+#             'npdb.sorting',
+#             'npdb.statistics']
+
+# and automatic
+packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
 
 setup(name='npdb',
       version='0.1',
-      description='npdb: NumPy-compatible large, persistent n-dimensional arrays on disk',
-      url='https://github.com/ryanwallace96/npdb',
+      description='npdb: Parallel NumPy-like interface for large n-dimensional arrays on disk.',
+      url='https://github.com/ryancwallace/npdb',
       author='Ryan Wallace',
       author_email='ryanwallace@college.harvard.edu',
       license='MIT',
